34cc2b87f2bdfa99fdc461f5cc89ddb83d6f0fb7
kaleb <kalebwondimu33@gmail.com>
2025-11-10 15:08:18 +0300

    menu component , routing done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import HomePage from "./pages/homepage/HomePage";
+import { Route, Routes } from "react-router-dom";
+const HatsPage = (props) => (
+  <div>
+    {console.log(props)}
+    <h1>HatsPage</h1>
+  </div>
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop/hats" element={<HatsPage />} />
+      </Routes>
+      \
     </div>
   );
 }
---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Homepage.scss";
+import Directory from "../../components/menu-item/directory/Directory";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
