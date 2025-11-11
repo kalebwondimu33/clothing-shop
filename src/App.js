35f01ebf68c9a0486a1d897f04a454617523472b
kaleb <kalebwondimu33@gmail.com>
2025-11-11 13:51:36 +0300

    adding header compoent and linds to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/Header";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/shop/" element={<ShopPage />} />
