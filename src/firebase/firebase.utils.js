0700c76f600917b3ec4cdca37461d3748c410a29
kaleb <kalebwondimu33@gmail.com>
2025-11-23 15:07:42 +0300

    the firebase configuration moved to the enviroment variable

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const config = {
-  apiKey: "AIzaSyDL2-n5OCLeAch8Rc3eVJZkmIRQ25DxQs8",
-  authDomain: "crown-db-d7fd3.firebaseapp.com",
-  projectId: "crown-db-d7fd3",
-  storageBucket: "crown-db-d7fd3.firebasestorage.app",
-  messagingSenderId: "967043883866",
-  appId: "1:967043883866:web:f7b28665fc1c3265d64963",
-  measurementId: "G-63YRNH0QDZ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,10 @@ import rootReducer from "./root-rducer";
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-const middlewares = [logger];
+const middlewares = [];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 export default { store, persistor };
